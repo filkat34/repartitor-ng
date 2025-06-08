3c6f74d54b8a86c3748f09ff2b71e094d0a5beee
filkat34 <filippos29@hotmail.com>
2025-06-08 17:47:38 +0200

    undefined values exception fixes

--- src/app/gestion-services/gestion-services.ts
+++ src/app/gestion-services/gestion-services.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Enseignant } from '../models/enseignant';
+import { IndexedDb } from '../indexed-db';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-gestion-services',
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './gestion-services.html',
   styleUrl: './gestion-services.css'
 })
--- src/app/models/enseignant.ts
+++ src/app/models/enseignant.ts
@@ -5,4 +5,5 @@ export interface Enseignant {
   ors: number; // Obligation réglementaire de service en heures à 100%
   quotite: number; // Quotité de service en pourcentage (0-100)
   service_plancher: number; // Service d'enseignement plancher prenant en compte la quotité
+  classes?: Array <{no: number; nom: string}>; // no = nb de classes de même niveau, nom = nom du niveau
 }
